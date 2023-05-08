63cdb0cb49a59c590d27edaf341d3181415a0931
Gerald <gerald@blackberryhill.net>
2023-05-08 14:13:23 -0500

    added everything

---
+++ Develop/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+var uniqid = require('uniqid');
+const noteData = require('./db/db.json');
+const { readAndAppend, readFromFile } = require('./helpers/fsUtils');
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+
+//added get path to main page
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+
+//added get path to notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+
+//allows to get the data from the notes
+
+
+
+app.get('/api/notes', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+
+app.post('/api/notes', (req, res) => {
+    const { noteTitle, note,} = req.body;
+    if (noteTitle && note) {
+        const newNote = {
+            noteTitle,
+            note,
+            note_id: uniqid(),
+        }
+        
+        readAndAppend(newNote, './db/db.json');
+        
+        
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting review');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
